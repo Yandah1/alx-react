ee594c3600a6e4e4c4bd85f2352d182e22d6d67f
Yandah1 <nonku003@gmail.com>
2024-05-07 01:17:10 +0200

    entry: {     main: path.resolve(__dirname, './js/dashboard_main.js'),   },

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,24 @@
+const $ = require('jquery');
+const _ = require('lodash');
+
+let count = 0;
+
+$("<p>Holberton Dashboard</p>").appendTo('body');
+$("<p>Dashboard data for the students</p>").appendTo('body');
+$("<button>Click here to get started</button>").appendTo('body');
+$("<p id='count'></p>").appendTo('body');
+$("<p>Copyright - Holberton School</p>").appendTo('body');
+
+
+function updateCounter() {
+    count++;
+    const countParagraph = document.getElementById('count');
+    countParagraph.textContent = `${count} clicks on the button`;
+}
+
+const Counter = _.debounce(updateCounter, 300);
+
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.querySelector('button');
+    button.addEventListener('click', Counter);
+});
