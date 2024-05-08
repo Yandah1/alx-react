68ef50bf5ffc5a82d201686bcc5bc070fabdf933
Yandah1 <nonku003@gmail.com>
2024-05-08 23:55:54 +0200

    done

---
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import logo from './holberton-logo.jpeg';
+import { getFooterCopy, getFullYear } from './utils';
+import Notifications from './Notifications';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="Holberton Logo" className="App-logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <hr className="separator" />
+        <p>Login to access the full dashboard</p>
+      </div>
+      <footer className="App-footer">
+        <hr className="separator" />
+        <p>{getFooterCopy(false)}</p>
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+       {/* Render Notifications component */}
+       <Notifications />
+    </div>
+  );
+}
+
+export default App;
