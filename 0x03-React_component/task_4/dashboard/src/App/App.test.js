1625ddd4f507ea77a7785a238e99a059a012522c
Yandah1 <nonku003@gmail.com>
2024-05-16 04:44:51 +0200

    . Write a test for the HOC

---
+++ 0x03-React_component/task_4/dashboard/src/App/App.test.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import App from './App';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import Notifications from '../Notifications/Notifications';
+import Login from '../Login/Login';
+
+describe('Test App.js', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('Renders App without crashing', () => {
+    expect(wrapper.exists()).toBeTruthy();
+  });
+
+  it('App component contains Notifications component', () => {
+    expect(wrapper.find(Notifications)).toHaveLength(1);
+  });
+
+  it('App component contains Header component', () => {
+    expect(wrapper.find("Header")).toHaveLength(1);
+  });
+
+  it('App component contains Login component', () => {
+    expect(wrapper.find("Login")).toHaveLength(1);
+  });
+
+  it('App component contains Footer component', () => {
+    expect(wrapper.find("Footer")).toHaveLength(1);
+  });
+
+  it('test to check that CourseList is not displayed inside App', () => {
+    expect(wrapper.find("CourseList")).toHaveLength(0);
+  });
+});
+
+describe("Testing <App isLoggedIn={true} />", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App isLoggedIn={true} />);
+  });
+
+  it("the Login component is not included", () => {
+    expect(wrapper.find('Login')).toHaveLength(0);
+  });
+
+  it("the CourseList component is included", () => {
+    expect(wrapper.find('CourseList').exists()).toBeTruthy();
+  });
+});
+
+describe('Testing <App logOut={logOutMock} />', () => {
+  let logOutMock;
+  let alertMock;
+  let wrapper;
+
+  beforeEach(() => {
+    logOutMock = jest.fn();
+    alertMock = jest.spyOn(window, 'alert').mockImplementation(() => {});
+    wrapper = shallow(<App logOut={logOutMock} />);
+  });
+
+  afterEach(() => {
+    alertMock.mockRestore();
+  });
+
+  it('calls logOut and displays alert when Ctrl+H is pressed', () => {
+    const instance = wrapper.instance();
+    const event = { preventDefault: jest.fn(), ctrlKey: true, key: 'h' };
+    instance.handleKeyDown(event);
+
+    expect(event.preventDefault).toHaveBeenCalled();
+    expect(logOutMock).toHaveBeenCalled();
+    expect(alertMock).toHaveBeenCalledWith('Logging you out');
+  });
+});
+
+
+describe('render', () => {
+
+  // Renders the Notification component with the listNotifications prop.
+  it('should render the Notification component with the listNotifications prop', () => {
+    const isLoggedIn = true;
+    const wrapper = mount(<App isLoggedIn={isLoggedIn} />);
+    expect(wrapper.find(Notifications).prop('listNotifications')).toEqual(App.listNotifications);
+  });
+
+  // If this.props.isLoggedIn is null or undefined, renders the Login component inside a BodySectionWithMarginBottom component with the title prop "Log in to continue".
+  it('should render the Login component inside a BodySectionWithMarginBottom component with the title prop "Log in to continue" when this.props.isLoggedIn is null or undefined', () => {
+    const isLoggedIn = null;
+    const wrapper = shallow(<App isLoggedIn={isLoggedIn} />);
+    expect(wrapper.find(BodySectionWithMarginBottom).prop('title')).toEqual('Log in to continue');
+    expect(wrapper.find(Login)).toHaveLength(1);
+  });
+});
