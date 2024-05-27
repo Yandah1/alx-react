16533db387a9ec1d22041fa3d58d5630942233e5
Yandah1 <nonku003@gmail.com>
2024-05-27 19:56:04 +0200

    notifications fixed

--- 0x05-react_state/task_0/dashboard/src/App/App.js
+++ 0x05-react_state/task_0/dashboard/src/App/App.js
@@ -31,43 +31,49 @@ const styles = StyleSheet.create({
 });
 
 class App extends React.Component {
-  static listCourses = [
-    { id: 1, name: 'ES6', credit: 60 },
-    { id: 2, name: 'Webpack', credit: 20 },
-    { id: 3, name: 'React', credit: 40 }
-  ];
-
-  static listNotifications = [
-    { id: 1, value: "New course available", type: "default" },
-    { id: 2, value: "New resume available", type: "urgent" },
-    { id: 3, html: { __html: getLatestNotification() }, type: "urgent" },
-  ];
-
+  
   constructor(props) {
     super(props);
     this.isLoggedIn = props.isLoggedIn;
     this.logOut = props.logOut;
     this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.listCourses = [
+      {id: 1, name: 'ES6', credit: 60},
+      {id: 2, name: 'Webpack', credit: 20},
+      {id: 3, name: 'React', credit: 40}
+    ];
+  
+    this.listNotifications = [
+      {id: 1, value: "New course available", type: "default"},
+      {id: 2, value: "New resume available", type: "urgent"},
+      {id: 3, html: {__html: getLatestNotification()}, type: "urgent"},
+    ];
     this.state = {
       displayDrawer: false
     };
+
     this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
     this.handleHideDrawer = this.handleHideDrawer.bind(this);
   }
 
   handleDisplayDrawer() {
-    this.setState({ displayDrawer: true });
+    this.setState({
+      displayDrawer: true
+    });
   }
 
   handleHideDrawer() {
-    this.setState({ displayDrawer: false });
+    this.setState({
+      displayDrawer: false
+    });
   }
+
   handleKeyDown(e) {
-    e.preventDefault();
     if (e.ctrlKey && e.key === 'h') {
+      e.preventDefault();
       alert("Logging you out");
       this.logOut();
-    }
+    }  
   }
 
   componentDidMount() {
