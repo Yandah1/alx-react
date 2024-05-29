fca4604c5b2913d429c1c2ccc49a5ea612cafd87
Yandah1 <nonku003@gmail.com>
2024-05-29 18:53:37 +0200

    list

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,13 +1,6 @@
-import { Map } from 'immutable';
+import {nfromJS } from 'immutable';
 export default function accessImmutableObject(object, array) {
-    return array.reduce((acc, current) => {
-      if (Map.isMap(acc)) {
-        return acc.get(current);
-      } else if (typeof acc === 'object' && acc !== null) {
-        return acc[current];
-      } else {
-        return undefined;
-      }
-    }, object);
-  }
-  
+    const immutableObject = fromJS(object);
+    return immutableObject.getIn(array);
+}
+   
