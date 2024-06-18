183d5ea7a53efcb9e56e4a9b1900f33be5e1d4d2
Yandah1 <nonku003@gmail.com>
2024-06-19 01:09:02 +0200

    combine reducers

--- 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/index.js
@@ -4,9 +4,13 @@ import App from "./App/App";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import { thunk } from "redux-thunk";
-import rootReducer from "./reducers/uiReducer";
+import rootReducer from "./reducers/rootReducer";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <React.StrictMode>
